bd65e5bc2ce425855e014384916afcc91cd6eadb
yeoj1n <yeojinkim94@gmail.com>
2020-03-31 01:10:01 +0900

    update covid19 global statistics

--- src/App.js
+++ src/App.js
@@ -1,26 +1,116 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.getCountryData = this.getCountryData.bind(this);
+  }
+  state = {
+    confirmed: 0,
+    recovered: 0,
+    deaths: 0,
+    countries: [],
+    selectCountryData : null
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  async getData() {
+    const resApi = await axios.get("https://covid19.mathdro.id/api");
+    const resCountries = await axios.get("https://covid19.mathdro.id/api/countries")
+    // countries 대신 index만 보여서 배열에 넣어서 처리
+    const countries = [];
+    for(let i=0; i<resCountries.data.countries.length; i++)
+      countries.push(resCountries.data.countries[i].name);
+
+    this.setState({
+      confirmed: resApi.data.confirmed.value,
+      recovered: resApi.data.recovered.value,
+      deaths: resApi.data.deaths.value,
+      countries
+    });
+  };
+
+  renderCountryOptions() {
+    return this.state.countries.map((country,idx) => {
+      return <option key={idx}>
+        {country}
+      </option>
+    });
+  }
+
+  async getCountryData(e) {
+    const country = e.target.value;
+    const countryApi = await axios.get("https://covid19.mathdro.id/api/countries" + "/" + country)
+
+    this.setState({
+      selectCountryData : {
+        confirmed: countryApi.data.confirmed.value,
+        recovered: countryApi.data.recovered.value,
+        deaths: countryApi.data.deaths.value
+      }
+    })
+  };
+  render() {
+    const { confirmed, recovered, deaths, selectCountryData  } = this.state;
+    const { getCountryData } = this;
+
+    return (
+      <div className="container">
+        <h1>Crona19 Statistics</h1>
+
+        <h2> All Countries </h2>
+        <div className="flex">
+          <div className="box confirmed">
+            <h3>Confirmed case</h3>
+            <h4>{confirmed}</h4>
+          </div>
+
+          <div className="box recovered">
+            <h3>Recovered case</h3>
+            <h4>{recovered}</h4>
+          </div>
+
+          <div className="box deaths">
+            <h3>Deaths case</h3>
+            <h4>{deaths}</h4>
+          </div>
+        </div>
+
+      {/*
+        <h2> Select Country </h2>
+        <select onChange={getCountryData}>
+          {this.renderCountryOptions()}
+        </select>
+      
+         {selectCountryData &&
+        (
+          <div className="flex">
+          <div className="box confirmed">
+            <h3>Confirmed case</h3>
+            <h4>{confirmed}</h4>
+          </div>
+
+          <div className="box recovered">
+            <h3>Recovered case</h3>
+            <h4>{recovered}</h4>
+          </div>
+
+          <div className="box deaths">
+            <h3>Deaths case</h3>
+            <h4>{deaths}</h4>
+          </div>
+        </div>
+        )} */}
+      </div>
+    )
+  }
 }
 
 export default App;
